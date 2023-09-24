8fbb2d35761aa6122f2eb7f32f22d8deaaab22ee
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-24 03:49:24 +0100

    add: if conditions stricture

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+
+let chiffreUn = "10";
+let chiffreDeux = 0;
+let chiffreTrois = 20;
+let chiffreQ
+let maVariable = chiffreUn != chiffreDeux + "10" && chiffreTrois >= 20 - chiffreQ;
+let comparaison = chiffreDeux < chiffreQ;
+
+console.log (maVariable);
+console.log(comparaison);
+console.log("this console.log of ... you know , what ");
+console.log(chiffreQ);
+console.log("this typeof... you know , what ");
+console.log(typeof chiffreQ);
+console.log("is undefined equal to zero or false");
+console.log(comparaison);
+
+
+
+alert (chiffreUn);
+console.log()
+
+if (true){
+    alert("first condition ")
+}else if(chiffreDeux) {
+    alert("second condition")
+}else if(chiffreQ){
+    alert("nothing to see")
+
+}else{
+    alert("this is the very nagate condition")
+}
+
+
+let maMoyenne = prompt("quelle a été ta moyenne ");
+
+if(0>maMoyenne){
+    alert("qu'est ta bralné là !! merde")
+}else if(0<maMoyenne && maMoyenne<=5){
+
+    alert("attends , tu n'estudie pas ou quoi ?")
+}else if(6<maMoyenne && maMoyenne<10){
+
+    alert("hum , c'est quoi ca ..  , tu est capabe de faire mieux ?")
+}else if(maMoyenne==10){
+
+    alert("ca c'est pille la moyenne ! fais plus")
+}else if(10 < maMoyenne && maMoyenne < 20){
+
+    alert("c'est bon , je te fellicite , vas toujours come cela ")
+}else if(maMoyenne == 20){
+
+    alert("surper mon Fils , Max!!!")
+
+}else if(maMoyenne>20){
+
+    alert("petit !! je t'ai à l'àeil")
+
+}else {
+    alert("je ne sais pas quelle est ta moyenne")
+}
